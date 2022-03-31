48c0664f36068dfb52eb41898f941877d1938459
Vincent Ahrens <vincent.ahrens@gmx.net>
2022-03-31 14:29:16 +0200

    again

--- frontend/src/components/StockInfo.tsx
+++ frontend/src/components/StockInfo.tsx
@@ -1,11 +1,10 @@
-
 export default function StockInfo (){
 
 
 return (
     <div className="stock-info">
         <ul>
-            <span>Aktien-Kürzel</span> <span>Platzhalter</span> <span>Letzter Kurs </span>
+            <span>Aktien-Kürzel</span> <span>(Platzhalter)</span> <span>Letzter Kurs </span>
         </ul>
     </div>
 
