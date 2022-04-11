18273f52976ab8a11c4dab0db19a0bd711a785a4
Vincent Ahrens <vincent.ahrens@gmx.net>
2022-04-11 18:01:15 +0200

    frontend

--- frontend/src/components/StockInfo.tsx
+++ frontend/src/components/StockInfo.tsx
@@ -3,9 +3,7 @@ export default function StockInfo (){
 
 return (
     <div className="stock-info">
-        <ul>
-            <span>name</span> <span>price$</span> <span>date</span> <span>quantity</span>
-        </ul>
+
     </div>
 
 )
